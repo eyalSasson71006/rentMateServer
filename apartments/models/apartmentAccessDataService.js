808217c17069e296850d1ae18d26236b2f567c67
eyal71006 <eyal71006@gmail.com>
2024-09-20 12:26:45 +0300

    edit like and delete functions and routes

--- apartments/models/apartmentAccessDataService.js
+++ apartments/models/apartmentAccessDataService.js
@@ -38,4 +38,41 @@ const getUsersApartments = async (userId) => {
     }
 };
 
-module.exports = { createApartment, getApartments, getApartmentById, getUsersApartments };
+const updateApartment = async (apartmentId, updatedApartment) => {
+    try {
+        let apartment = await Apartment.findByIdAndUpdate(apartmentId, updatedApartment, { new: true });
+        return apartment;
+    } catch (error) {
+        createError("Mongoose ", error);
+    }
+};
+
+const likeApartment = async (apartmentId, userId) => {
+    try {
+        let apartment = await Apartment.findById(apartmentId);
+        if (!apartment) {
+            const error = new Error("an apartment with this ID cannot be not found in the database");
+            return createError("Mongoose", error, 404);
+        }
+        if (apartment.likes.includes(userId)) {
+            apartment.likes = apartment.likes.filter(id => id != userId);
+        } else {
+            apartment.likes.push(userId);
+        }
+        await apartment.save();
+        return apartment;
+    } catch (error) {
+        createError("Mongoose ", error);
+    }
+};
+
+const deleteApartment = async (apartmentId) => {
+    try {
+        let apartment = await Apartment.findByIdAndDelete(apartmentId);
+        return apartment;
+    } catch (error) {
+        createError("Mongoose ", error);
+    }
+};
+
+module.exports = { createApartment, getApartments, getApartmentById, getUsersApartments, updateApartment, likeApartment, deleteApartment };
--- apartments/routes/apartmentRestController.js
+++ apartments/routes/apartmentRestController.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createApartment, getApartments, getApartmentById } = require("../models/apartmentAccessDataService");
+const { createApartment, getApartments, getApartmentById, updateApartment, likeApartment, deleteApartment } = require("../models/apartmentAccessDataService");
 const { handleError } = require("../../utils/handleErrors");
 const auth = require("../../auth/authService");
 const normalizeApartment = require("../helpers/normalizeApartment");
@@ -35,6 +35,49 @@ router.get("/:id", async (req, res) => {
         res.send(apartment);
     } catch (error) {
         handleError(res, 400, error.message);
+    }
+});
+
+router.put("/:id", auth, async (req, res) => {
+    try {
+        const userInfo = req.user;
+        const { id } = req.params;
+        const newApartment = req.body;
+        const fullApartmentFromDb = await getApartmentById(id);
+        if (userInfo._id != fullApartmentFromDb.owner.ownerId && !userInfo.isAdmin) {
+            return handleError(res, 403, "Authorization Error: Only the user who created the apartment or admin can update its details");
+        }
+        let apartment = await normalizeApartment(newApartment, userInfo._id);
+        apartment = await updateApartment(id, apartment);
+        res.send(apartment);
+    } catch (error) {
+        handleError(res, error.status || 400, error.message);
+    }
+});
+
+router.patch("/:id", auth, async (req, res) => {
+    try {
+        const userInfo = req.user;
+        const { id } = req.params;
+        let apartment = await likeApartment(id, userInfo._id);
+        res.send(apartment);
+    } catch (error) {
+        handleError(res, error.status || 400, error.message);
+    }
+});
+
+router.delete("/:id", auth, async (req, res) => {
+    try {
+        const { id } = req.params;
+        const userInfo = req.user;
+        const fullApartmentFromDb = await getApartmentById(id);
+        if (userInfo._id != fullApartmentFromDb.owner.ownerId && !userInfo.isAdmin) {
+            return handleError(res, 403, "Authorization Error: Only the user who created the apartment or admin can delete it");
+        }
+        let apartment = await deleteApartment(id);
+        res.send(apartment);
+    } catch (error) {
+        handleError(res, error.status || 400, error.message);
     }
 });
 
