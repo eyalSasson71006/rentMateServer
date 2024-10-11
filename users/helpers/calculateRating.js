0f819721161e2b00d18be48dda70d7294c8f5ef9
eyal71006 <eyal71006@gmail.com>
2024-10-11 14:27:33 +0300

    fixed calculate rating bug where 0 reviews would break the code

--- users/helpers/calculateRating.js
+++ users/helpers/calculateRating.js
@@ -2,10 +2,12 @@ const calculateRating = (apartments) => {
     let reviews = [];
     apartments = apartments.forEach(apartment => reviews.push(...apartment.reviews));
     let rating = 0;
-    reviews.forEach(review => {
+    reviews.forEach(review => {        
         rating += review.rating;
-    });
-    rating = rating / reviews.length;
+    });    
+    rating = (rating / reviews.length) || 0;
+    console.log(rating);
+    
     return { reviews, rating };
 };
 
